407b1c9fc55c1006c4aa0922562f0f6a606f644e
BorisovMaxim <soulmaxim@gmail.com>
2021-06-05 17:36:43 +0400

    Homework_1 done

---
+++ src/ru/geekbrains/homeworks/Obstacle.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.homeworks;
+
+public abstract class Obstacle {
+    public abstract boolean pass(Participants participant);
+}
---
+++ src/ru/geekbrains/homeworks/Participants.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.homeworks;
+
+public interface Participants {
+    void run();
+    void jump();
+    double getMaxJump();
+    double getMaxRun();
+    String getName();
+}
